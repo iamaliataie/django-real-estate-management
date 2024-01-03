8b38ff19b6db306dace471362b4bb2677a80cc28
Ali Ahmad Ataie <iamaliataie@gmail.com>
2024-01-03 21:16:11 +0430

    Update views.py

--- property/views.py
+++ property/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import Property, PropertyType
-
 from inquiry.forms import InquiryForm
 
 from search.models import SearchCriteria
